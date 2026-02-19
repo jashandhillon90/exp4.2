9fd557c734a72823ad717abf41f0b19930686fe3
Jashan <24bai70540@cuchd.in>
2026-02-19 22:01:02 +0530

    Switched from Vercel to Render

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import cardRoutes from "./routes/cards.routes.js";
-import serverless from "serverless-http";
 
 const app = express();
 
 app.use(express.json());
 
+// Connect MongoDB
 connectDB();
 
+// Routes
 app.use("/cards", cardRoutes);
 
-export const handler = serverless(app);
+// IMPORTANT FOR RENDER
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
